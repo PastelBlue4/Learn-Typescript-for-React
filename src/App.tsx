f74fb401e80162e74a159d47c1d643759432c2d7
pastelblue <pastelblue0721@gmail.com>
2022-02-26 21:32:19 +0900

    2022-02-26 Learn type for event, state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import styled from "styled-components";
-import Circle from './Circle';
+import React, { useState } from 'react';
 
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const{
+      currentTarget: {value}
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello",value);
+  };
   return(
     <div>
-      <Circle bgColor="tomato"/>
-      <Circle bgColor="skyblue" borderColor="blue" />
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" placeholder='username'/>
+        <button>Log in</button>
+      </form>
     </div>
     );
 }
