82b7f3fe8d067fc941de358a56b6114a016c5035
pastelblue <pastelblue0721@gmail.com>
2022-03-01 23:49:05 +0900

    2022-03-01 setup for after course

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-import { lightTheme } from "./theme";
+import { Theme } from "./theme";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={Theme}>
       <App />
       </ThemeProvider>
   </React.StrictMode>,
